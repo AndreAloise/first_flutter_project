24db0031f42fcb4da42a0c085b5595949aa589d9
AndreAloise <andre.aloise.77@gmail.com>
2023-10-12 17:22:36 -0300

    Criando mais alguns containers

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,28 @@ class MyApp extends StatelessWidget {
         alignment: AlignmentDirectional.center,
         children: [
           Container(
-            color: Colors.amber,
+            color: Colors.white,
+          ),
+          Container(
+            color: Colors.black,
+            width: 300,
+            height: 300,
+          ),
+          Container(
+            color: Colors.red,
             width: 150,
             height: 150,
           ),
           Container(
-            color: Colors.green,
-            width: 50,
-            height: 50,
+            color: Colors.blue,
+            width: 75,
+            height: 75,
           ),
+          Container(
+            color: Colors.yellow,
+            width: 37,
+            height: 37,
+          )
         ],
       ),
     );
