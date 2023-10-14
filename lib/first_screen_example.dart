0a266daa13d8be752fc1b74b54bcdfa9d4401647
AndreAloise <andre.aloise.77@gmail.com>
2023-10-14 14:25:47 -0300

    Criando arquivo para a primeira tela e ajustando código na main para chamá-la

---
+++ lib/first_screen_example.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class FirstScreenExample extends StatelessWidget {
+  const FirstScreenExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: column1(),
+    );
+  }
+
+  Column column1() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              color: Colors.blue,
+              width: 75,
+              height: 75,
+            ),
+            Container(
+              color: Colors.yellow,
+              width: 37,
+              height: 37,
+            )
+          ],
+        ),
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              color: Colors.yellow,
+              width: 75,
+              height: 75,
+            ),
+            Container(
+              color: Colors.blue,
+              width: 37,
+              height: 37,
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 50,
+              height: 50,
+            ),
+            Container(
+              color: Colors.orange,
+              width: 50,
+              height: 50,
+            ),
+            Container(
+              color: Colors.purple,
+              width: 50,
+              height: 50,
+            )
+          ],
+        ),
+        Container(
+          color: Colors.amber,
+          height: 30,
+          width: 300,
+          child: const Text(
+            'Diamante amarelo',
+            style: TextStyle(color: Colors.black, fontSize: 22),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: buttonFunc1,
+          child: const Text('Aperte o botão!'),
+        ),
+      ],
+    );
+  }
+
+  void buttonFunc1() {
+    print('Você apertou o botão!');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_project/first_screen_example.dart';
 import 'package:flutter/material.dart';
 
 import 'exercise_01.dart';
@@ -16,87 +17,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: Container(color: Colors.white, child: column1()),
-      home: invoqueExercise01(context),
+      home: invoqueFirstScreenExample(context),
+      //home: invoqueExercise01(context),
     );
   }
 
-  Column column1() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Container(
-              color: Colors.blue,
-              width: 75,
-              height: 75,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 37,
-              height: 37,
-            )
-          ],
-        ),
-        Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Container(
-              color: Colors.yellow,
-              width: 75,
-              height: 75,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 37,
-              height: 37,
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.orange,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.purple,
-              width: 50,
-              height: 50,
-            )
-          ],
-        ),
-        Container(
-          color: Colors.amber,
-          height: 30,
-          width: 300,
-          child: const Text(
-            'Diamante amarelo',
-            style: TextStyle(color: Colors.black, fontSize: 22),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        ElevatedButton(
-          onPressed: buttonFunc1,
-          child: const Text('Aperte o botão!'),
-        ),
-      ],
-    );
-  }
-
-  void buttonFunc1() {
-    print('Você apertou o botão!');
+  Widget invoqueFirstScreenExample(BuildContext context) {
+    FirstScreenExample firstScreen = const FirstScreenExample();
+    return firstScreen.build(context);
   }
 
   Widget invoqueExercise01(BuildContext context) {
