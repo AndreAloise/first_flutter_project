11c6e61a6939b3ad9de0213d13d20853d9b0891c
AndreAloise <andre.aloise.77@gmail.com>
2023-10-15 00:50:26 -0300

    Extraíndo código para métodos auxiliares

--- lib/exercise_02.dart
+++ lib/exercise_02.dart
@@ -93,90 +93,96 @@ class _TaskEx02State extends State<TaskEx02> {
         ),
         Column(
           children: [
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                      child: Image.network(
-                        widget.photoUrl,
-                        fit: BoxFit.cover,
-                      )),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                          width: 200,
-                          child: Text(widget.taskName,
-                              style: const TextStyle(
-                                  fontSize: 24,
-                                  overflow: TextOverflow.ellipsis))),
-                      Row(
-                        children: [
-                          Icon(Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 1)
-                                  ? Colors.blue
-                                  : Colors.blue[100]),
-                          Icon(Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 2)
-                                  ? Colors.blue
-                                  : Colors.blue[100]),
-                          Icon(Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 3)
-                                  ? Colors.blue
-                                  : Colors.blue[100]),
-                          Icon(Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 4)
-                                  ? Colors.blue
-                                  : Colors.blue[100]),
-                          Icon(Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 5)
-                                  ? Colors.blue
-                                  : Colors.blue[100]),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(height: 60, width: 60, child: _levelUpTaskButton())
-                ],
-              ),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 200,
-                  child: LinearProgressIndicator(
-                    color: Colors.white,
-                    value: (widget.difficulty > 0)
-                        ? (level / widget.difficulty) / 10
-                        : 1,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Nivel: $level',
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ),
-            ])
+            _taskCardContainer(),
+            _levelProgressRow(),
           ],
         )
       ],
     );
+  }
+
+  Container _taskCardContainer() {
+    return Container(
+      color: Colors.white,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+              color: Colors.black26,
+              width: 72,
+              height: 100,
+              child: Image.network(
+                widget.photoUrl,
+                fit: BoxFit.cover,
+              )),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  width: 200,
+                  child: Text(widget.taskName,
+                      style: const TextStyle(
+                          fontSize: 24, overflow: TextOverflow.ellipsis))),
+              Row(
+                children: [
+                  Icon(Icons.star,
+                      size: 15,
+                      color: (widget.difficulty >= 1)
+                          ? Colors.blue
+                          : Colors.blue[100]),
+                  Icon(Icons.star,
+                      size: 15,
+                      color: (widget.difficulty >= 2)
+                          ? Colors.blue
+                          : Colors.blue[100]),
+                  Icon(Icons.star,
+                      size: 15,
+                      color: (widget.difficulty >= 3)
+                          ? Colors.blue
+                          : Colors.blue[100]),
+                  Icon(Icons.star,
+                      size: 15,
+                      color: (widget.difficulty >= 4)
+                          ? Colors.blue
+                          : Colors.blue[100]),
+                  Icon(Icons.star,
+                      size: 15,
+                      color: (widget.difficulty >= 5)
+                          ? Colors.blue
+                          : Colors.blue[100]),
+                ],
+              ),
+            ],
+          ),
+          Container(height: 60, width: 60, child: _levelUpTaskButton())
+        ],
+      ),
+    );
+  }
+
+  Row _levelProgressRow() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 200,
+          child: LinearProgressIndicator(
+            color: Colors.white,
+            value:
+                (widget.difficulty > 0) ? (level / widget.difficulty) / 10 : 1,
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          'Nivel: $level',
+          style: const TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    ]);
   }
 
   ElevatedButton _levelUpTaskButton() {
