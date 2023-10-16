58e00e0afcc4b65d6cf7cc7837709f00c8cec972
AndreAloise <andre.aloise.77@gmail.com>
2023-10-16 01:08:22 -0300

    Usando SingleChildScrollView para não causar overflow na tela com a quantidade de campos e ter o teclado sobrepondo a tela. Adicionando tipo específico de teclado para cada campo.

--- lib/form_screen/form_screen_page.dart
+++ lib/form_screen/form_screen_page.dart
@@ -17,82 +17,88 @@ class _FormScreenPage extends State<FormScreenPage> {
     return Scaffold(
       appBar: AppBar(title: const Text('Nova Tarefa')),
       body: Center(
-        child: Container(
-          color: null,
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 3)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nome',
-                    fillColor: Colors.white70,
-                    filled: true,
+        child: SingleChildScrollView(
+          child: Container(
+            color: null,
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.name,
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Difficulty',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: difficultyController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Difficulty',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (text) {
-                    setState(() {});
-                  },
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Image',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
-              ),
-              Container(
-                width: 100,
-                height: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(width: 2, color: Colors.blue),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    imageController.text,
-                    errorBuilder: (BuildContext context, Object exception,
-                        StackTrace? stackTrace) {
-                      return Image.asset('assets/images/no-photo-icon.png');
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.url,
+                    onChanged: (text) {
+                      setState(() {});
                     },
-                    fit: BoxFit.cover,
+                    controller: imageController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Image',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(onPressed: () {}, child: const Text('Adicionar!'))
-            ],
+                Container(
+                  width: 100,
+                  height: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    border: Border.all(width: 2, color: Colors.blue),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stackTrace) {
+                        return Image.asset('assets/images/no-photo-icon.png');
+                      },
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Adicionar!'))
+              ],
+            ),
           ),
         ),
       ),
