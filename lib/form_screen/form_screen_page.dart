b082f933bf9cb82d513b4cdb86d9b4a5c15760c7
AndreAloise <andre.aloise.77@gmail.com>
2023-10-15 23:33:30 -0300

    Criando arquivo para página de formulário Stateful

---
+++ lib/form_screen/form_screen_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FormScreenPage extends StatefulWidget {
+  const FormScreenPage({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreenPage> createState() => _FormScreenPage();
+}
+
+class _FormScreenPage extends State<FormScreenPage> {
+  bool shouldApplyOpacity = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
