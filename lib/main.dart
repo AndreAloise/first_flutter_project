9086bb9eb1f8021ebdbf432bfc76393e9f2974c0
AndreAloise <andre.aloise.77@gmail.com>
2023-10-16 18:00:58 -0300

    Removendo métodos desnecessários e chamando apenas o Inherited

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:first_flutter_project/exercises/exercise_01.dart';
-import 'package:first_flutter_project/exercises/exercise_03.dart';
+import 'package:first_flutter_project/data/task_cards_inherited.dart';
 import 'package:first_flutter_project/task_cards/task_cards_page.dart';
-import 'package:first_flutter_project/exercises/first_screen_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,30 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: invoqueExercise03(context),
-      home: invoqueTaskCards(),
-      //home: invoqueFirstScreenExample(context),
-      //home: invoqueExercise01(context),
+      home: TaskCardsInherited(child: const TaskCardsPage()),
     );
-  }
-
-  Widget invoqueFirstScreenExample(BuildContext context) {
-    FirstScreenExample firstScreen = const FirstScreenExample();
-    return firstScreen.build(context);
-  }
-
-  Widget invoqueExercise01(BuildContext context) {
-    Exercise01 exe01 = const Exercise01();
-    return exe01.build(context);
-  }
-
-  Widget invoqueTaskCards() {
-    TaskCardsPage taskCards = const TaskCardsPage();
-    return taskCards;
-  }
-
-  Widget invoqueExercise03(BuildContext context) {
-    Exercise03 exercise03 = const Exercise03();
-    return exercise03.build(context);
   }
 }
