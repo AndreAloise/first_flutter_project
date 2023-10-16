79d29d15d639e70d92f761cab347551cb78e4c86
AndreAloise <andre.aloise.77@gmail.com>
2023-10-16 00:43:07 -0300

    Adicionando tratamento para caso de não ter imagem ou texto com imagem estar incorreto, espaçando os campos do formulário.

--- lib/form_screen/form_screen_page.dart
+++ lib/form_screen/form_screen_page.dart
@@ -26,6 +26,8 @@ class _FormScreenPage extends State<FormScreenPage> {
               borderRadius: BorderRadius.circular(10),
               border: Border.all(width: 3)),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -81,6 +83,10 @@ class _FormScreenPage extends State<FormScreenPage> {
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                     imageController.text,
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace? stackTrace) {
+                      return Image.asset('assets/images/no-photo-icon.png');
+                    },
                     fit: BoxFit.cover,
                   ),
                 ),
