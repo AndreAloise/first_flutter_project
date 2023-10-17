822b42dddc5389904f11bfc68f7ed0ba1060da92
AndreAloise <andre.aloise.77@gmail.com>
2023-10-17 01:43:15 -0300

    Implementando lógica de salvar ou atualizar uma task de acordo com seu nome.

--- lib/data/dao/task_card_dao.dart
+++ lib/data/dao/task_card_dao.dart
@@ -12,7 +12,52 @@ class TaskCardDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  saveTask(TaskCard taskCard) async {}
+  ///If doesn't save or update, try to change code to return those methods instead of just invoking
+  saveOrUpdate(TaskCard taskCard) async {
+    debugPrint('INIT - TaskCardDao.saveOrUpdate');
+
+    final Database dataBase = await getDatabase();
+    List<TaskCard> existsTask = await findTasksByName(taskCard.taskName);
+    Map<String, dynamic> taskMap = _toMap(taskCard);
+
+    if (existsTask.isEmpty) {
+      _saveTask(taskMap, dataBase);
+    } else {
+      _updateTask(taskCard.taskName, taskMap, dataBase);
+    }
+
+    debugPrint('END - TaskCardDao.saveOrUpdate');
+  }
+
+  _saveTask(Map<String, dynamic> taskMap, Database dataBase) async {
+    debugPrint('INIT - TaskCardDao.saveTask');
+
+    await dataBase.insert(_tableName, taskMap);
+
+    debugPrint('END - TaskCardDao.saveTask');
+  }
+
+  _updateTask(
+      String taskName, Map<String, dynamic> taskMap, Database dataBase) async {
+    debugPrint('INIT - TaskCardDao._updateTask');
+
+    await dataBase.update(_tableName, taskMap,
+        where: '$_name = ?', whereArgs: [taskName]);
+
+    debugPrint('END - TaskCardDao._updateTask');
+  }
+
+  Map<String, dynamic> _toMap(TaskCard taskCard) {
+    debugPrint('Converting TaskCard into Map');
+
+    final Map<String, dynamic> taskMap = Map();
+    taskMap[_name] = taskCard.taskName;
+    taskMap[_image] = taskCard.photoPath;
+    taskMap[_difficulty] = taskCard.difficulty;
+
+    debugPrint('Converted Map: $taskMap');
+    return taskMap;
+  }
 
   Future<List<TaskCard>> findAll() async {
     debugPrint('INIT - TaskCardDao.findAll');
